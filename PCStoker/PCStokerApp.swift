152ae2d2d930a249a8194c0d47c2ecca0d2e86ca
jessica <jeanrual94@gmail.com>
2024-09-30 13:17:06 -0500

    Initial Commit

---
+++ PCStoker/PCStokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PCStokerApp.swift
+//  PCStoker
+//
+//  Created by Jessica Alba on 30/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PCStokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PCStokerTests/PCStokerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PCStokerTests.swift
+//  PCStokerTests
+//
+//  Created by Jessica Alba on 30/09/24.
+//
+
+import Testing
+@testable import PCStoker
+
+struct PCStokerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
